b07257e789cf86309416016742cdd556a257c2c8
Justin Garcia <justingarcia1901@gmail.com>
2024-03-20 20:21:31 -0400

    home.jsx framework, started on grapahql utils

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,1 +1,29 @@
 //**The main landing page of the site. */
+import { useQuery } from '@apollo/client'
+import ProductList from '../components/ProductViewer'
+import { QUERY_PRODUCTS} from '..utils/queries'
+
+const Home = () => {
+  const { loading, data } = useQuery(QUERY_PRODUCTS);
+  const products = data?.products || [];
+
+
+  return (
+    <main>
+      <div /* className = 'container' */>
+        {loading ? (
+          <div /* className = skeleton */>Loading...</div>
+        ) : (
+          <>
+            <div /* className = 'banner section styling' */></div>
+            <div /* className = 'hero section' */></div>
+            <div /* className = 'banner section styling' */></div>
+            <div /* className = '' */></div>
+          </>
+        )}
+      </div>
+    </main>
+  );
+};
+
+export default Home;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,12 @@
 // GraphQL queries for fetching data from the server.
+import { gql } from '@apollo/client';
+
+export const QUERY_PRODUCTS =gql `
+query allProducts {
+    products {
+        _id
+        name
+        description
+    }
+}
+`
