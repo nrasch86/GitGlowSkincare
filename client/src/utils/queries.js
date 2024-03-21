05c742cc2c8c2c4cd9c01abd697690a5771536e2
Justin Garcia <justingarcia1901@gmail.com>
2024-03-20 20:55:42 -0400

    imported utils scripts

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,12 +1,34 @@
 // GraphQL queries for fetching data from the server.
 import { gql } from '@apollo/client';
 
-export const QUERY_PRODUCTS =gql `
-query allProducts {
+export const QUERY_PRODUCTS = gql`
+{
+    products {
+        _id
+        name
+        image
+        description
+        price
+    }
+}
+`;
+
+export const QUERY_USER = gql`
+{ 
+    user {
+firstName
+lastName
+orders {
+    _id
     products {
         _id
         name
         description
+        price
+        quantity
+        image
     }
-}
-`
+   }
+  }
+ }
+`;
