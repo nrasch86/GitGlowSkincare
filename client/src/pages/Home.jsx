5809f44842196f21597d4b2e465ba684a075159f
Justin Garcia <justingarcia1901@gmail.com>
2024-03-20 21:54:31 -0400

    moved navbar to components folder, updated procutlist name, updated home.jsx frameworks, updated app.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 //**The main landing page of the site. */
-import { useQuery } from "@apollo/client";
-import ProductList from "../components/ProductList";
-import ProductReviews from "../components/Product Reviews";
-import { QUERY_PRODUCTS } from "..utils/queries";
-import { QUERY_REVIEWS } from "..utils/queries";
+import { useQuery } from '@apollo/client'
+import ProductList from '../components/ProductViewer'
+import { QUERY_PRODUCTS} from '..utils/queries'
 
 const Home = () => {
+  const { loading, data } = useQuery(QUERY_PRODUCTS);
+  const products = data?.products || [];
+
+
   return (
     <main>
       <div /* className = 'container' */>
@@ -13,30 +15,10 @@ const Home = () => {
           <div /* className = skeleton */>Loading...</div>
         ) : (
           <>
-            <div /* className = 'banner section styling' */>
-              <img></img>
-            </div>
-            <div /* className = 'hero section' */>
-              <div /* className = 'product shot grid' */></div>
-              <div /* className = 'product shot grid' */></div>
-              <div /* className = 'product shot grid' */></div>
-              <div /* className = 'product shot grid' */></div>
-            </div>
-            <div /* className = 'magazine logos' */>
-              <ul>
-                <li>
-                  <img src="" alt="randomMagazine 1" />
-                  <img src="" alt="randomMagazine 2" />
-                  <img src="" alt="randomMagazine 3" />
-                </li>
-              </ul>
-            </div>
-            <div /* className = 'shop Hero section' */>
-              <ProductList />
-            </div>
-            <div /* className = 'reviews section' */>
-              <ProductReviews />
-            </div>
+            <div /* className = 'banner section styling' */></div>
+            <div /* className = 'hero section' */></div>
+            <div /* className = 'banner section styling' */></div>
+            <div /* className = '' */></div>
           </>
         )}
       </div>
