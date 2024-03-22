0c415946383b8512b02e25f40d9f761372abea65
bladeasf <125907788+bladeasf@users.noreply.github.com>
2024-03-21 20:55:18 -0400

    login jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,1 +1,66 @@
-//**The login form page. */
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+//import './Login.css';//
+const Login = () => {
+    const [email, setEmail] = useState(''); 
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+    const [login, { error, data }] = useMutation(LOGIN_USER);
+useEffect(()=>{
+  if(Auth.loggedIn()|| data){
+    navigate('/')
+  }
+
+}, [data, navigate])
+
+    const handleLogin = async (e) => {
+      e.preventDefault();
+
+      try {
+        const { data } = await login({variables: {email: email, password: password}}); 
+ 
+        Auth.login(data.login.token);
+        
+      } catch (error) {
+        console.error('Login failed:', error);
+      }
+    };
+  
+    const navigateToRegister = () => {
+      navigate('/Signup');
+    };
+
+    return (
+        <div className="LoginWrapper">
+           
+            <div className="LoginContainer">
+                <h2 className="LoginTitle">Log in</h2>
+                 {error && (
+              <div className="my-3 p-3 bg-danger text-white">
+                {error.message}
+              </div>
+            )}
+                <form onSubmit={handleLogin}>
+                    <input
+                        className="Input"
+                        type="email" 
+                        placeholder="Email" 
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input
+                        className="Input"
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <button className="LoginButton" type="submit">LOG IN</button>
+                </form>
+                <button className="SignUpButton" onClick={navigateToSignup}>SIGN UP</button>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
