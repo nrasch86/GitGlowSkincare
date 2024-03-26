b45f682654b69e45cb2625788d3eb7e44e397cfa
Evan Chang <evanchang17812@hotmail.com>
2024-03-25 22:16:06 -0400

    edited connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,12 @@
-//Configuration for the database connection.
-const { MongoClient } = require('mongodb');
+const mongoose = require("mongoose");
 let uri = "mongodb+srv://admin:admin123@gitglow.wlti1oa.mongodb.net/?retryWrites=true&w=majority&appName=GitGlow"
+mongoose.connect(uri);
 
-MongoClient.connect(process.env.MONGODB_URI || uri);
+const db = mongoose.connection;
 
-module.exports = MongoClient.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
+module.exports = db;
