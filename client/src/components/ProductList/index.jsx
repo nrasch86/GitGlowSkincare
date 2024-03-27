f2be60430fee615863c3f5c78a0c0fb58abd3b97
Justin Garcia <justingarcia1901@gmail.com>
2024-03-26 23:40:13 -0400

    productlist tweaks

--- client/src/components/ProductList/index.jsx
+++ client/src/components/ProductList/index.jsx
@@ -2,19 +2,13 @@ import { Link } from 'react-router-dom';
 
 const ProductList = ({ products, productName }) => {
     if (!products.length) {
-        return <h3>No Thoughts Yet</h3>;
+        return <h3>No Products Yet</h3>;
       }
     return (<div>
         <h3>{productName}</h3>
         {products &&
           products.map((product) => (
             <div key={product._id}>
-              <h4>
-                {thought.thoughtAuthor} <br />
-                <span>
-                  had this thought on {product.createdAt}
-                </span>
-              </h4>
               <div>
                 <p>{product.description}</p>
               </div>
@@ -25,7 +19,7 @@ const ProductList = ({ products, productName }) => {
               </Link>
             </div>
           ))}
-          
+
       </div>);
 }
 
