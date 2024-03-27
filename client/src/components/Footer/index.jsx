3dd0dc3abb2ccd36951fd21b2ab8bda2d24a9302
bladeasf <125907788+bladeasf@users.noreply.github.com>
2024-03-26 21:57:06 -0400

    footer

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,9 +1,11 @@
+import React from 'react';
+import "../Footer/index.css"
 const Footer = () => {
-  return (<div>
-  <div>Hello</div>
-  <div>Hello</div>
-  <div>Hello</div>
-  <div>Hello</div>
+  return (<div className="footer-container" >
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  <div>4</div>
   </div>
   );
 };
