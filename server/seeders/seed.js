d6b0693b5bae365f78428bf9ad654f57980f42f2
Evan Chang <evanchang17812@hotmail.com>
2024-03-26 23:38:49 -0400

    Fixed seeding

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,16 +1,17 @@
 //The main script to run the seeders and populate the database with initial data.
-
 const db = require('../config/connection');
 const { Product } = require('../models');
 const productSeeds = require('./productSeeds.json');
+//const { User } = require('../models');
+//const profileSeeds = require('./profileSeeds.json');
 const cleanDB = require('./cleanDB');
 
 db.once('open', async () => {
   try {
     await cleanDB('Product', 'products');
-    
+   // await cleanDB('Users', 'users');
     await Product.create(productSeeds);
-
+  //  await User.create(profileSeeds);
     console.log('all done!');
     process.exit(0);
   } catch (err) {
